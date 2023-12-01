f8f5f17a272b3111bfbdc11ad80fe2aa5bbedc65
hammahutz <62766637+hammahutz@users.noreply.github.com>
2023-12-01 19:19:59 +0100

    Connected to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import dotenv from "dotenv";
 import express, { Express, ErrorRequestHandler } from "express";
+import "colors"
+
+import connectDb from "./data/connectDb.js";
 import goalRouter from "./routes/goalRoutes.js";
 import errorHandler from "./middleware/errorMiddlewar.js";
 
 dotenv.config();
-
 const port = process.env.PORT;
+
+connectDb();
 const app: Express = express();
 
 app.use(express.json())
@@ -15,4 +19,4 @@ app.use("/api/goals", goalRouter);
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server start on port ${port}`));
+app.listen(port, () => console.log(`Server start on port ${port}`.cyan.underline));
