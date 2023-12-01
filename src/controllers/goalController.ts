4d3da9c52d3042888324171795307a289e55d47f
hammahutz <62766637+hammahutz@users.noreply.github.com>
2023-12-01 18:43:14 +0100

    Added async and errorhandling

--- src/controllers/goalController.ts
+++ src/controllers/goalController.ts
@@ -1,41 +1,46 @@
 import { Request, Response } from "express";
-
-// @desc Get all goals
-// @route GET /api/goals
-// @access Private
-const getGoals = (req: Request, res: Response) => {
-    const message = "Get goals"
+import expressAsyncHandler from "express-async-handler";
+/**
+ * @desc Get all goals
+ * @route GET /api/goals
+ * @access Private
+ */
+const getGoals = expressAsyncHandler(async (req: Request, res: Response) => {
+  const message = "Get goals";
   res.status(200).json({ message: message });
   console.log(message);
-
-};
+});
 
 // @desc Get a goal
 // @route GET /api/goals/:id
 // @access Private
-const getGoal = (req: Request, res: Response) => {
+const getGoal = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Get Goal with id ${req.params.id}` });
-};
+});
 
 // @desc Set a goal
 // @route SET /api/goals
 // @access Private
-const setGoal = (req: Request, res: Response) => {
-  res.status(200).json({ message: "Set goal" });
-};
+const setGoal = expressAsyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  res.status(200).json({ message: `Set goal: ${req.body.text}` });
+});
 
 // @desc Update a goal
 // @route Update /api/goals/:id
 // @access Private
-const updateGoal = (req: Request, res: Response) => {
+const updateGoal = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc Delete a goal
 // @route Detlete /api/goals/:id
 // @access Private
-const deleteGoal = (req: Request, res: Response) => {
+const deleteGoal = expressAsyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 export { getGoals, getGoal, setGoal, updateGoal, deleteGoal };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
-import express, { Express } from "express";
+import express, { Express, ErrorRequestHandler } from "express";
 import goalRouter from "./routes/goalRoutes.js";
+import errorHandler from "./middleware/errorMiddlewar.js";
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app: Express = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", goalRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server start on port ${port}`));
